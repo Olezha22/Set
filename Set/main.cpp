c331f7f9eae883f608f7010642fff01b33916f60
hehuasho <165146294+hehuasho@users.noreply.github.com>
2024-04-02 19:17:48 +0300

    Update main.cpp

--- Set/main.cpp
+++ Set/main.cpp
@@ -1,17 +1,52 @@
 #include "set.h"
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-	Set<int> maksiSet;
+    Set<int> maksiSet;
 
-	maksiSet.add(3);
-	maksiSet.add(2);
-	maksiSet.add(1);
-	maksiSet.add(3);
-	
+    maksiSet.add(3);
+    maksiSet.add(2);
+    maksiSet.add(1);
 
-	maksiSet.print();
+    cout << "Initial set:" << endl;
+    maksiSet.print();
 
-	return 0;
+    cout << "Size of set: " << maksiSet.getSize() << endl;
+
+    int elementToCheck = 2;
+    if (maksiSet.contain(elementToCheck))
+        cout << elementToCheck << " is in the set" << endl;
+    else
+        cout << elementToCheck << " is not in the set" << endl;
+
+    int elementToRemove = 3;
+    maksiSet.remove(elementToRemove);
+    cout << "After removing " << elementToRemove << ":" << endl;
+    maksiSet.print();
+
+    Set<int> secondSet;
+    secondSet.add(3);
+    secondSet.add(4);
+    secondSet.add(5);
+
+    Set<int> unionResult = maksiSet.unionSet(secondSet);
+    cout << "Union of sets:" << endl;
+    unionResult.print();
+
+    Set<int> intersectionResult = maksiSet.intersect(secondSet);
+    cout << "Intersection of sets:" << endl;
+    intersectionResult.print();
+
+    Set<int> differenceResult = maksiSet.difference(secondSet);
+    cout << "Difference of sets:" << endl;
+    differenceResult.print();
+
+    Set<int> symmetricDifferenceResult = maksiSet.sym_difference(secondSet);
+    cout << "Symmetric difference of sets:" << endl;
+    symmetricDifferenceResult.print();
+
+    return 0;
 }
